ce6ae84785c4fe79f21f2aa35472784b0e84ce90
Travis Smith <travis.kenneth.smith@gmail.com>
2022-07-11 23:54:16 -0500

    git ignore

--- containert/src/oci/pull.rs
+++ containert/src/oci/pull.rs
@@ -2,6 +2,9 @@ use std::process::Command;
 
 pub fn pull_image(image_dir: String, image_name: String) -> String {
     let output = Command::new("skopeo").arg("copy").arg(image_name).arg(image_dir).output().expect("failed to execute process");
+    if !output.status.success() {
+        println!("{}", "Command executed with failing error code");
+    }
     let output_string = String::from_utf8(output.stdout).ok();
     return output_string.unwrap();
 }
